d941dea7aa12489759e01c84f6f343a91d51f0f1
kavinKSH <kavin14795@gmail.com>
2024-05-28 14:17:18 +0530

    [Future] create networking functions

---
+++ Git-Pull_prequests/HTTPClient.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPClient.swift
+//  Git-Pull_prequests
+//
+//  Created by Kavin on 28/05/24.
+//
+
+import Foundation
+
+class HTTPClient {
+    init() {}
+    
+    static func networking() {
+        print("Datas from server")
+    }
+}
