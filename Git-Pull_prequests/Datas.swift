4e91e2d5ed48c726eeea67efce7d83f3b25335a5
kavinKSH <kavin14795@gmail.com>
2024-05-29 12:04:06 +0530

    [Future] User Models
    (cherry picked from commit b0be09a3cabc07de6989f01b2d86be08595d6b07)

---
+++ Git-Pull_prequests/Datas.swift
@@ -0,0 +1,14 @@
+//
+//  Datas.swift
+//  Git-Pull_prequests
+//
+//  Created by Kavin on 29/05/24.
+//
+
+import Foundation
+
+struct Datas {
+    
+    static let userName = "Sathish"
+    static let password = "1234567"
+}
