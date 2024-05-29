3978b65a48879d083b9d511424479f66522a1cf3
kavinKSH <kavin14795@gmail.com>
2024-05-29 12:07:19 +0530

    [Future]Receive datas

--- Git-Pull_prequests/ViewController.swift
+++ Git-Pull_prequests/ViewController.swift
@@ -27,5 +27,9 @@ class ViewController: UIViewController {
     func addSomeFutures() {
         print("Check if any updates UIdesigns, then Updates")
     }
+    
+    func receiveUserDetails() {
+        print("Recevie user Details Datas")
+    }
 }
 
