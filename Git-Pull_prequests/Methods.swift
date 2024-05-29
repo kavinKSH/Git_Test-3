167b395f9f42bb39fdaaeb66e6b1ef17d2784334
kavinKSH <kavin14795@gmail.com>
2024-05-29 13:27:41 +0530

    [future] create HTTPMethods

--- Git-Pull_prequests/Methods.swift
+++ Git-Pull_prequests/Methods.swift
@@ -1,5 +1,5 @@
 //
-//  HTTPMethods.swift
+//  Methods.swift
 //  Git-Pull_prequests
 //
 //  Created by Kavin on 29/05/24.
@@ -8,8 +8,6 @@
 import Foundation
 
 enum HTTPMethods: String {
-    
-    case get    = "GET"
-    case post   = "POST"
-    
+    case get  = "GET"
+    case post = "POST"
 }
