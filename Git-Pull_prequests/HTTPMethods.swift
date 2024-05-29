971756398488b6ebca2d2126cab5846f3265b8d0
kavinKSH <kavin14795@gmail.com>
2024-05-29 13:25:05 +0530

    [Future] create http methos

---
+++ Git-Pull_prequests/HTTPMethods.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethods.swift
+//  Git-Pull_prequests
+//
+//  Created by Kavin on 29/05/24.
+//
+
+import Foundation
+
+enum HTTPMethods: String {
+    
+    case get    = "GET"
+    case post   = "POST"
+    
+}
