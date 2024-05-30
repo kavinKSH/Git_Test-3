09e4f6a79ab5cd8e05d8523581d3c2d8ccd135dc
kavinKSH <kavin14795@gmail.com>
2024-05-30 18:27:40 +0530

    [Future] create detailVC

---
+++ Git-Pull_prequests/DetailVC/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  Git-Pull_prequests
+//
+//  Created by Kavin on 30/05/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Print user details")
+    }
+}
